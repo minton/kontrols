6c0a90ad878485f560d434a1c8756a46473ac23e
Michael Minton <michaelcminton@gmail.com>
2019-01-06 00:08:57 -0600

    Build the assembly before staging for nuget

--- Build.bat
+++ Build.bat
@@ -1,2 +1,3 @@
+MSBuild.exe .\src\kontrols.sln /t:Rebuild /p:Configuration=Release
 copy "C:\Dev\kontrols\src\kontrols\bin\Release\kontrols.dll" "C:\Dev\kontrols\nuget\lib\net45\kontrols.dll"
 nuget.exe pack "C:\dev\kontrols\nuget\kontrols.nuspec"
