fae2966524e1218e3ac49727e2cba6abf8f297c2
Michael Minton <michael@minton.io>
2020-07-13 23:29:52 -0500

    Introduce ControlBox support to SimpleBorderForm

--- src/kontrols/SimpleBorderForm.Designer.cs
+++ src/kontrols/SimpleBorderForm.Designer.cs
@@ -28,7 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnMinimize = new kontrols.HoverImageButton();
+            this.btnMaximize = new kontrols.HoverImageButton();
+            this.btnClose = new kontrols.HoverImageButton();
             this.SuspendLayout();
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnMinimize.HoverBackColor = System.Drawing.Color.White;
+            this.btnMinimize.HoverForeColor = System.Drawing.Color.Empty;
+            this.btnMinimize.HoverImage = null;
+            this.btnMinimize.Image = null;
+            this.btnMinimize.Location = new System.Drawing.Point(675, 3);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(25, 25);
+            this.btnMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnMinimize.TabIndex = 2;
+            // 
+            // btnMaximize
+            // 
+            this.btnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnMaximize.HoverBackColor = System.Drawing.Color.White;
+            this.btnMaximize.HoverForeColor = System.Drawing.Color.Empty;
+            this.btnMaximize.HoverImage = null;
+            this.btnMaximize.Image = null;
+            this.btnMaximize.Location = new System.Drawing.Point(700, 3);
+            this.btnMaximize.Name = "btnMaximize";
+            this.btnMaximize.Size = new System.Drawing.Size(25, 25);
+            this.btnMaximize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnMaximize.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnClose.HoverBackColor = System.Drawing.Color.White;
+            this.btnClose.HoverForeColor = System.Drawing.Color.Empty;
+            this.btnClose.HoverImage = null;
+            this.btnClose.Image = null;
+            this.btnClose.Location = new System.Drawing.Point(725, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 25);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnClose.TabIndex = 0;
             // 
             // SimpleBorderForm
             // 
@@ -36,6 +81,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(754, 465);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnMaximize);
+            this.Controls.Add(this.btnClose);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -47,5 +95,9 @@
         }
 
         #endregion
+
+        private HoverImageButton btnClose;
+        private HoverImageButton btnMaximize;
+        private HoverImageButton btnMinimize;
     }
 }
--- src/kontrols/SimpleBorderForm.cs
+++ src/kontrols/SimpleBorderForm.cs
@@ -23,6 +23,8 @@ namespace kontrols
         public static IntPtr HTBOTTOMRIGHT => (IntPtr)17;
 
         Color _borderColor;
+        Color _hoverControlBoxForeColor;
+        Color _hoverControlBoxBackColor;
 
         [Browsable(true)]
         public bool ShowTitle { get; set; }
@@ -33,6 +35,70 @@ namespace kontrols
             SetStyle(ControlStyles.ResizeRedraw, true);
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
+            Load += (_, __) =>
+            {
+                if (!ControlBox)
+                {
+                    btnClose.Visible =
+                        btnMinimize.Visible =
+                            btnMaximize.Visible = false;
+                    return;
+                }
+
+                SetupButton(btnClose, DrawCloseButton);
+                SetupButton(btnMaximize, DrawMaximizeButton);
+                SetupButton(btnMinimize, DrawMinimizeButton);
+            };
+            btnClose.Click += (_, __) => Close();
+            btnMaximize.Click += (_, __) => WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+            btnMinimize.Click += (_, __) => WindowState = FormWindowState.Minimized;
+        }
+
+        void SetupButton(HoverImageButton button, Func<Color, Image> getImage) 
+        {
+            button.BorderStyle = BorderStyle.None;
+            button.Image = getImage(BorderColor);
+            button.HoverImage = getImage(BackColor);
+            button.HoverBackColor = BorderColor;
+            button.HoverForeColor = BackColor;
+            button.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        Image DrawCloseButton(Color color)
+        {
+            var img = new Bitmap(25, 25);
+            var g = Graphics.FromImage(img);
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            using var p = new Pen(color, 1);
+            g.DrawLine(p, 7, 7, 15, 15);
+            g.DrawLine(p, 7, 15, 15, 7);
+            return img;
+        }
+
+        Image DrawMaximizeButton(Color color)
+        {
+            var img = new Bitmap(25, 25);
+            var g = Graphics.FromImage(img);
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            using var p = new Pen(color, 1);
+            g.DrawRectangle(p, 9, 5, 10, 10);
+            g.DrawRectangle(p, 7, 7, 10, 10);
+            return img;
+        }
+        Image DrawMinimizeButton(Color color)
+        {
+            var img = new Bitmap(25, 25);
+            var g = Graphics.FromImage(img);
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            using var p = new Pen(color, 1);
+            g.DrawLine(p, 9, 7, 18, 7);
+            return img;
         }
 
         public Color BorderColor
@@ -41,6 +107,36 @@ namespace kontrols
             set
             {
                 _borderColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// The ForeColor used when the mouse is over the control box.
+        /// </summary>        
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color HoverControlBoxForeColor
+        {
+            get => _hoverControlBoxForeColor.IsEmpty ? BackColor : _hoverControlBoxForeColor;
+            set
+            {
+                _hoverControlBoxForeColor = value;
+                Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// The BackColor used when the mouse is over the control box.
+        /// </summary>        
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public Color HoverControlBoxBackColor
+        {
+            get => _hoverControlBoxBackColor.IsEmpty ? BackColor : _hoverControlBoxBackColor;
+            set
+            {
+                _hoverControlBoxBackColor = value;
                 Invalidate();
             }
         }
@@ -60,7 +156,7 @@ namespace kontrols
         {
             base.OnPaintBackground(e);
             using var p = new Pen(BorderColor, 2);
-            e.Graphics.DrawRectangle(p, 2, 2, Width-4, Height -4);
+            e.Graphics.DrawRectangle(p, 2, 2, Width - 4, Height - 4);
         }
 
         Rectangle CaptionArea => new Rectangle(24, 4, ClientSize.Width - 48, CaptionHeight);
